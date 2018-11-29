99da68faa073fcb697fa40aa2b059ce120e9b18a
drboboev <boboevdr@gmail.com>
2018-11-29 10:07:13 +0300

    Created class

---
+++ SortWords.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace drboboev\SortWords;
+
+class SortWords
+{
+    public function sort($string)
+    {
+        $words = mb_split("\s", $string);
+
+        $result_words = array();
+
+        foreach ($words as $word) {
+
+            $word = mb_strtolower($word);
+
+            $letters = preg_split('//u', $word, -1, PREG_SPLIT_NO_EMPTY);
+
+            sort($letters);
+            $word = implode($letters, "");
+
+            $result_words[] = $word;
+        }
+
+        $result_string = implode($result_words, " ");
+
+        return $result_string;
+    }
+}
